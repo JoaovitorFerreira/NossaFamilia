96b57b65188cef231c9d1c3503f2ec3e22113044
João Vítor <32188614+JoaovitorFerreira@users.noreply.github.com>
2021-05-02 14:36:29 -0300

    CriacaoDoProjeto

---
+++ Assets/Scripts/BomberMan.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class BomberMan : MonoBehaviour
+{
+
+    public Animator animator;
+    public Rigidbody2D rigidbody;
+    public float speed;
+    public float timeRemaining = 10;
+    public bool isWalking = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (timeRemaining > 0)
+        {
+            timeRemaining -= Time.deltaTime;
+            isWalking = false;
+        }
+        else
+        {
+            timeRemaining = 10;
+            isWalking = true;
+
+        }
+
+    }
+
+    void FixedUpdate()
+    {
+        Walk();
+    }
+
+    void Walk()
+    {
+        if (isWalking)
+        {
+            rigidbody.velocity = new Vector2(-1 * speed, rigidbody.velocity.y);
+            //personagem vira pros lados
+                transform.eulerAngles = new Vector2(0, 180);
+                animator.SetInteger("Transition", 1);
+
+
+        }
+        else
+        {
+                animator.SetInteger("Transition", 0);
+        }
+
+    }
+
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public Animator animator;
+    public Rigidbody2D rigidbody;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        Walk();
+
+
+    }
+
+    void Walk()
+    {
+        float direcao = Input.GetAxis("Horizontal");
+        rigidbody.velocity = new Vector2(direcao * speed, rigidbody.velocity.y);
+
+        if (direcao > 0)
+        {
+            transform.eulerAngles = new Vector2(0,0);
+            animator.SetInteger("Transition", 1);
+        }
+
+        if (direcao < 0)
+        {
+            transform.eulerAngles = new Vector2(0, 180);
+            animator.SetInteger("Transition", 1);
+        }
+        if (direcao ==0)
+        {
+            animator.SetInteger("Transition", 0);
+        }
+
+    }
+
+}
